af6fc06625829bcd892f6d522d137f7cb0c5ae17
soyounglee026 <71009039+soyounglee026@users.noreply.github.com>
2025-11-15 22:47:24 +0900

    [Bronze I] Title: 세로읽기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,17 @@
+s1 = input()
+s2 = input()
+s3 = input()
+s4 = input()
+s5 = input()
+
+for i in range(15):
+  if i in range(len(s1)):
+    print(s1[i], end="")
+  if i in range(len(s2)):
+    print(s2[i], end="")
+  if i in range(len(s3)):
+    print(s3[i], end="")
+  if i in range(len(s4)):
+    print(s4[i], end="")
+  if i in range(len(s5)):
+    print(s5[i], end="")
